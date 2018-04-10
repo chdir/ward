c2899b8e616b2bcd68a54274f75c6edfb837d89b
AlexanderR <alexander.r@gmx.com>
2018-04-10 11:55:02 +0700

    Added libvips binaries

---
+++ bin/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# set -x
+
+/autogen.sh --without-libwebp --without-magick --without-OpenEXR --without-pangoft2 --without-tiff --disable-shared --enable-static --disable-magickload --disable-magicksave --without-matio --without-ppm --without-analyze --without-radiance --without-cfitsio --without-rsvg --disable-gtk-doc --disable-gtk-doc-html --without-poppler --without-x
