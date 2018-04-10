5ff384581f77450b44c28fa1c27cb5c64f12c71e
AlexanderR <alexander.r@gmx.com>
2018-04-10 12:04:18 +0700

    Update converter script

--- magick.sh
+++ magick.sh
@@ -2,7 +2,5 @@
 cd "$(dirname "$0")"
 exec {source}< $2 # source.jpg
 exec {target}> $4 # target.jpg
-LD_LIBRARY_PATH=./bin LD_PRELOAD=./bin/jail LANG=C ./bin/vips --vips-concurrency=1 resize "/proc/self/fd/${source}" "/proc/self/fd/${target}.jpeg" 1
-
-# SYSCALL_DEBUG=1 strace -eopen,openat -E LD_PRELOAD=./jail
+D_PRELOAD=./bin/jail TMPDIR=/var/www/upload/user/ VIPS_DISC_THRESHOLD=5m LANG=C ./bin/vips --vips-concurrency=1 resize "/proc/self/fd/${source}" "/proc/self/fd/${target}.jpeg" 1
 
